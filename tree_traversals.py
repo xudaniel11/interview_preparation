2fa92703646e2483cd01df4390c302e44cb41fb8
Jasmine Lee <t-jasl@microsoft.com>
2016-08-06 18:16:46 -0700

    add tree_traversals

---
+++ tree_traversals.py
@@ -0,0 +1,93 @@
+import unittest
+
+def level_order(node): # root, children
+	q = [node]
+	while len(q) > 0:
+		curr = q.pop(0)
+		print curr.val,
+		if curr.left:
+			q.append(curr.left)
+		if curr.right:
+			q.append(curr.right)
+	return node
+
+def inorder(node): #left, root, then right
+	if node != None:
+		if node.left:
+			inorder(node.left)
+		print node.val,
+		if node.right:
+			inorder(node.right)
+
+def preorder(node): #root, left, then right
+	if node != None:
+		print node.val,
+		if node.left:
+			preorder(node.left)
+		if node.right:
+			preorder(node.right)
+
+def postorder(node): #left, right, then root
+	if node != None:
+		if node.left:
+			postorder(node.left)
+		if node.right:
+			postorder(node.right)
+		print node.val,
+
+class BinaryTreeNode():
+	def __init__(self, val):
+		self.val = val
+		self.left = None
+		self.right = None
+
+class TestTreeTraversals(unittest.TestCase):
+	def test_level_order(self):
+		root = BinaryTreeNode(0)
+		root.left = BinaryTreeNode(1)
+		root.right = BinaryTreeNode(2)
+		root.left.left = BinaryTreeNode(3)
+		root.left.right = BinaryTreeNode(4)
+		print "should be 0, 1, 2, 3, 4: "
+		level_order(root) 
+		print "\n"
+
+	def test_inorder(self):
+		root = BinaryTreeNode('A')
+		root.left = BinaryTreeNode('B')
+		root.right = BinaryTreeNode('C')
+		root.right.left = BinaryTreeNode('F')
+		root.right.right = BinaryTreeNode('G')
+		root.left.left = BinaryTreeNode('D')
+		root.left.right = BinaryTreeNode('E')
+		print "should be D,B,E,A,F,C,G: "
+		inorder(root)
+		print "\n"
+
+	def test_preorder(self):
+		root = BinaryTreeNode('A')
+		root.left = BinaryTreeNode('B')
+		root.right = BinaryTreeNode('C')
+		root.right.left = BinaryTreeNode('F')
+		root.right.right = BinaryTreeNode('G')
+		root.left.left = BinaryTreeNode('D')
+		root.left.right = BinaryTreeNode('E')
+		print "should be A,B,D,E,C,F,G: "
+		preorder(root) 
+		print "\n"
+
+
+	def test_postorder(self):
+		root = BinaryTreeNode('A')
+		root.left = BinaryTreeNode('B')
+		root.right = BinaryTreeNode('C')
+		root.right.left = BinaryTreeNode('F')
+		root.right.right = BinaryTreeNode('G')
+		root.left.left = BinaryTreeNode('D')
+		root.left.right = BinaryTreeNode('E')
+		print "should be D,E,B,F,G,C,A: "
+		postorder(root) 
+		print "\n"
+
+if __name__ == '__main__':
+	unittest.main()
