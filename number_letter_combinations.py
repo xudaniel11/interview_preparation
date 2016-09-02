a9f3a265b17116250f1b50ed9d4420fc1e7860e7
Jasmine <jasminelee@berkeley.edu>
2016-09-01 21:05:52 -0700

    fix number_letter_combinations

--- number_letter_combinations.py
+++ number_letter_combinations.py
@@ -1,19 +1,17 @@
 """
-Given a mapping of alphabets to integers as follows: 
+A message containing letters from A-Z is being encoded to numbers using the following mapping:
 
-1 = A 
-2 = B 
-3 = C 
-... 
-26 = Z 
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+Given an encoded message containing digits, determine the total number of ways to decode it.
 
-Given any combination of the mapping numbers as string, return the number of ways in which the input string can be split into sub-strings and represented as character strings. For e.g. given 
-"111" -> "AAA", "AK", "KA" -> 3 
-Valid combinations are ({1,1,1}, {1,11},{11,1}) = 3 
-"11" -> "AA", "K" -> 2 
-Valid combinations are ({1,1},{11}) = 2 
-"123" -> "ABC", "LC", "AW" -> 3 
-Valid combinations are ({1,2,3},{1,23},{12,3}) = 3 
+Example :
+
+Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
+
+The number of ways decoding "12" is 2.
 
 You don't have to return all the mappings, only the number of valid mappings.
 
@@ -21,31 +19,72 @@ Follow up harder question: return all the mappings LOL
 """
 import unittest
 
+# initial solution doesn't take into account the fact that '0' doesn't
+# have a letter mapping
+
+
+def number_letter_combinations_WRONG(input_str):
+    cache = [1, 1]
+
+    for i in range(1, len(input_str)):
+        if int(input_str[i - 1] + input_str[i]) < 27:
+            cache.append(int(cache[i]) + int(cache[i - 1]))
+        else:
+            cache.append(int(cache[i]))
+    return cache[-1]
+
+
 def number_letter_combinations(input_str):
-	cache = [1, 1]
-	for i in range(1, len(input_str)):
-		if int(input_str[i-1] + input_str[i]) < 27:
-			cache.append(int(cache[i]) + int(cache[i-1]))
-		else:
-			cache.append(int(cache[i]))
-	return cache[-1]
+    n = len(input_str) + 1
+    # edge case when there are many 0's in the input. again, 0 doesn't correspond to
+    # an alphabet letter
+    if all(x == '0' for x in input_str) or input_str[0] == '0' or '00' in input_str:
+        return 0
+    if len(input_str) == 1:
+        return 1
+
+    count = [0] * n
+    # base case
+    count[0] = 1
+    count[1] = 1
+
+    for i in range(2, n):
+        if input_str[i - 1] > '0':
+            count[i] = count[i - 1]
+        # if we see a 0 reset our count to a prev amount
+        if input_str[i - 2] == '1' or (input_str[i - 2] == '2' and input_str[i - 1] < '7'):
+            count[i] += count[i - 2]
+    print count
+    return count[-1]
+
+print number_letter_combinations(raw_input())
+
 
 class TestNumberLetterCombinations(unittest.TestCase):
-	def test_case_1(self):
-		input_str = "123"
-		result = number_letter_combinations(input_str)
-		self.assertEqual(result, 3)
 
-	def test_case_2(self):
-		input_str = "1212"
-		result = number_letter_combinations(input_str)
-		self.assertEqual(result, 5)
+    def test_case_1(self):
+        input_str = "0"
+        result = number_letter_combinations(input_str)
+        self.assertEqual(result, 0)
 
-	def test_case_3(self):
-		input_str = "1324"
-		result = number_letter_combinations(input_str)
-		self.assertEqual(result, 4)
+    def test_case_2(self):
+        input_str = "1212"
+        result = number_letter_combinations(input_str)
+        self.assertEqual(result, 5)
 
+    def test_case_3(self):
+        input_str = "5163490394499093221199401898020270545859326357520618953580237168826696965537789565062429676962877038781708385575876312877941367557410101383684194057405018861234394660905712238428675120866930196204792703765204322329401298924190"
+        result = number_letter_combinations(input_str)
+        self.assertEqual(result, 0)
+
+    def test_case_4(self):
+        input_str = "8"
+        result = number_letter_combinations(input_str)
+        self.assertEqual(result, 1)
+
+    def test_case_5(self):
+        input_str = "32925665678138257423442343752148360796465852883409126159293254159974370974059818198867156827877059067081419274873853679038"
+        result = number_letter_combinations(input_str)
+        self.assertEqual(result, 0)
 if __name__ == '__main__':
-	unittest.main()
-
+    unittest.main()
