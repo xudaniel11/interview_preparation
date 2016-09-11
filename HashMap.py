aad62ee9cd5849744fd1c700f3235e7ed1ea0d03
Jasmine <jasminelee@berkeley.edu>
2016-09-10 19:17:15 -0700

    more hashMap work

--- HashMap.py
+++ HashMap.py
@@ -3,12 +3,13 @@ import unittest
 
 class Singly_LL_Node():
 
-    def __init__(self, val):
+    def __init__(self, key, val):
         """
         Args:
             val (obj): The value for this node.
             next (Singly_LL_Node): The link to the next node.
         """
+        self.key = key
         self.val = val
         self.next = None
         self.prev = None
@@ -27,46 +28,55 @@ class Singly_LL():
         self.tail = None
         self.size = 0
 
-    def add(self, val):
+    def add(self, key, val):
         """
         Args:
             val (obj): The value for a new node to be added to this linked list.
         """
         if self.head == None:
-            self.head = Singly_LL_Node(val)
+            self.head = Singly_LL_Node(key, val)
             self.tail = self.head
         else:
-            new_tail = Singly_LL_Node(val)
+            new_tail = Singly_LL_Node(key, val)
             self.tail.next = new_tail
             self.tail = new_tail
         self.size += 1
 
-    def remove(self, val):
+    def remove(self, key):
         """
         Args:
-            val (obj): The value for the node(s) to be removed. Every node with this val
-            will be removed from the linked list.
+            val (obj): The value for the node to be removed.
         """
         curr = self.head
         prev = None
         while curr:
-            if curr.val == val:
+            if curr.key == key:
                 self.size -= 1
                 if curr == self.head:
                     self.head = curr.next
-                    return curr
+                    return curr.val
                 elif not curr.next:
                     prev.next = curr.next
                     self.tail = prev
-                    return curr
+                    return curr.val
                 else:
                     prev.next = curr.next
                     temp = curr
                     curr = curr.next
-                    return temp
+                    return temp.val
             else:
                 prev = curr
                 curr = curr.next
+        return None
+
+    def get(self, key):
+        curr = self.head
+        while curr:
+            if curr.key == key:
+                return curr.val
+            else:
+                curr = curr.next
+        return None
 
     def print_LL(self):
         """
@@ -88,54 +98,42 @@ class HashMap():
 
     def __init__(self, size):
         """ Returns an instance of a HashMap with size number of buckets. """
-        self.items = [Singly_LL()] * size
+        self.num_buckets = size
+        self.array = [Singly_LL()] * self.num_buckets
         self.capacity = 0
-        self.size = size
+        self.load_factor = .70
+        self.threshold = (self.load_factor * self.num_buckets)
 
     def set(self, key, value):
-        if self.capacity >= self.size:
+        if self.capacity >= self.threshold:
+            # redistribute all existing entries
             return False
         """ Returns a boolean value indicating success/failure of the operation. """
         hash_code = hash(key)
-        hashed_index = hash_code % self.size
-        arr = self.items[hashed_index:] + self.items[:hashed_index]
-        for index in xrange(len(arr)):
-            if arr[index] == None:
-                self.items[index] = [hash_code, value]
-                self.capacity += 1
-                return True
-        return False
+        hashed_index = hash_code & self.num_buckets - 1
+        ll = self.array[hashed_index]
+        ll.add(key, value)
+        self.capacity += 1
+        return True
 
     def get(self, key):
         """ Return the value associated with the given key, or null if no value is set. """
         hash_code = hash(key)
-        hashed_index = hash_code % self.size
-        arr = self.items[hashed_index:] + self.items[:hashed_index]
-        for index in xrange(len(arr)):
-            if arr[index] != None and arr[index][0] == hash_code:
-                return arr[index][1]
-        return None
+        hashed_index = hash_code & self.num_buckets - 1
+        ll = self.array[hashed_index]
+        return ll.get(key)
 
     def delete(self, key):
         """ Delete the value associated with the given key,
         returning the value on success or null if the key has no value. """
         hash_code = hash(key)
-        hashed_index = hash_code % self.size
-        arr = self.items[hashed_index:] + self.items[:hashed_index]
-        for index in arr:
-            if arr[index] != None and arr[index][0] == hash_code:
-                if arr[index][1] == None:
-                    return None
-                else:
-                    result = arr[index][1]
-                    arr[index] = None
-                    self.capacity -= 1
-                    return result
-        return None
+        hashed_index = hash_code & self.num_buckets - 1
+        ll = self.array[hashed_index]
+        return ll.remove(key)
 
     def load(self):
         """ Returns a float value representing the load factor. """
-        return float(self.capacity / self.size)
+        return float(self.capacity / self.threshold)
 
 
 # class TestHashMap(unittest.TestCase):
