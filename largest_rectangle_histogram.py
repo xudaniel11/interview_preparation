5d71e181fd04d94a9ce43eb603d60a55427e67fe
Jasmine <jasminelee@berkeley.edu>
2016-09-21 19:55:52 -0700

    fix largest rectangle histogram

--- largest_rectangle_histogram.py
+++ largest_rectangle_histogram.py
@@ -3,68 +3,59 @@ Given n non-negative integers representing the histogram's bar height
 where the width of each bar is 1, find the area of largest rectangle
 in the histogram.
 """
+"""
+Given n non-negative integers representing the histogram’s bar height
+where the width of each bar is 1, find the area of largest rectangle
+in the histogram.
+"""
 import unittest
 
 
-def naive_largest_rectangle(histogram):
-    n = len(histogram)
-    possibilities = []
-    for i in range(n):
-        start = i
-        start_block = histogram[start]
-        end = i
-        for j in range(i + 1, n):
-            if histogram[j] < start_block:
-                break
-            end = j
-        length = (end - start) + 1
-        possibilities.append(start_block * length)
-    return max(possibilities)
-
-
-def largest_rectangle(histogram):
-    stack = [0]
+def largest_rectangle(bar_heights):
+    stack = []
     max_area = -1
-    for i in range(1, len(histogram)):
+    i = 0
+    histogram = bar_heights[:]
+    histogram.append(0)
+    limit = len(histogram)
+    while i < limit:
         if not stack or histogram[i] >= histogram[stack[-1]]:
             stack.append(i)
-        elif histogram[i] < histogram[stack[-1]]:
-            top = 0
-            while stack and histogram[i] < histogram[stack[-1]]:
-                top = stack.pop()
-                if not stack:
-                    area = histogram[top] * i
-                else:
-                    area = histogram[top] * (i - stack[-1] - 1)
-                max_area = max(max_area, area)
-            stack.append(i)
-
-    while stack:
-        top = stack.pop()
-        area = histogram[top] * (len(histogram) - top)
-        print histogram[top], len(histogram), top
-        max_area = max(max_area, area)
+            i += 1
+        else:
+            top = stack.pop()
+            if not stack:
+                area = histogram[top] * i
+            else:
+                area = histogram[top] * (i - stack[-1] - 1)
+            max_area = max(max_area, area)
     return max_area
 
 
 class TestLargestRectangle(unittest.TestCase):
 
-    # def test_1(self):
-    #     a = [90, 58, 69, 70, 82, 100, 13, 57, 47, 18]
-    #     result = largest_rectangle(a)
-    #     expected = 348
-    #     self.assertEqual(result, expected)
+    def test_1(self):
+        a = [90, 58, 69, 70, 82, 100, 13, 57, 47, 18]
+        result = largest_rectangle(a)
+        expected = 348
+        self.assertEqual(result, expected)
 
-    # def test_2(self):
-    #     a = [1, 2, 4, 2, 1, 3]
-    #     result = largest_rectangle(a)
-    #     expected = 6
-    #     self.assertEqual(result, expected)
+    def test_2(self):
+        a = [1, 2, 4, 2, 1, 3]
+        result = largest_rectangle(a)
+        expected = 6
+        self.assertEqual(result, expected)
 
     def test_3(self):
         a = [2, 2, 2, 6, 1, 5, 4, 2, 2, 2]
         result = largest_rectangle(a)
         expected = 10
+        self.assertEqual(result, expected)
+
+    def test_4(self):
+        a = [1, 2, 0, 5, 4, 3]
+        result = largest_rectangle(a)
+        expected = 9
         self.assertEqual(result, expected)
 if __name__ == "__main__":
     unittest.main()
