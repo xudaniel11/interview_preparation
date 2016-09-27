8a7fcc0953383a599a5d02551de1e3d72311155d
Jasmine <jasminelee@berkeley.edu>
2016-09-27 16:40:30 -0700

    add follow up to robot paths problem

--- robot_unique_paths.py
+++ robot_unique_paths.py
@@ -28,11 +28,39 @@ def get_num_unique_paths(M, N):
     return int(mat[0, 0])
 
 
+"""
+Follow up: 
+Imagine certain spots are "off limits," such that the robot cannot step
+on them. Design an algorithm to find a path for the robot from the top
+left to the bottom right.
+
+Solution: make blocks 0.
+"""
+
+
+def robot(grid):
+    m, n = grid.shape
+    for i in reversed(range(m - 1)):
+        for j in reversed(range(n - 1)):
+            if grid[i, j] == 0:
+                continue
+            else:
+                grid[i, j] = grid[i, j + 1] + grid[i + 1, j]
+    return grid[0, 0]
+
+
 class TestUniquePaths(unittest.TestCase):
 
     def test_1(self):
         result = get_num_unique_paths(3, 7)
         expected = 28
+        self.assertEqual(result, expected)
+
+    def test_blocks(self):
+        mat = np.ones((3, 4))
+        mat[1, 0] = 0
+        result = robot(mat)
+        expected = 6
         self.assertEqual(result, expected)
 
 if __name__ == "__main__":
